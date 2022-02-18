72403891eb98c2904fc3cf1f323f0b839dbc3e1b
Nerto Kageh <kagehnerto@yahoo.com>
2022-02-17 23:52:05 -0600

    Changed some of the original coding from API Hackwich.

--- API Calling Attempt 3/ContentView.swift
+++ API Calling Attempt 3/ContentView.swift
@@ -8,14 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var IMDB = [IMDB]()
+    @State private var showingAlert = false
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(IMDB) { IMDB in
+                NavigationLink(
+                    destination: Text(IMDB.width)
+                        .padding(),
+                    label: {
+                        Text(IMDB.imageURL)
+                    })
+            }
+            .navigationTitle("Programming Jokes")
+        }
+        .onAppear(perform: {
+            getIMDB()
+        })
+        .alert(isPresented: $showingAlert, content: {
+            Alert(title: Text("Loading Error"),
+                  message: Text("There was a problem loading the data"),
+                  dismissButton: .default(Text("OK")))
+        })
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func getIMDB() {
+        let apiKey = "?rapidapi-key=(e466d8cfeamsh994ebcf54622aeep17993cjsn2fbf6b8204a6)"
+        
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["imageURL"].stringValue
+                        let width = item["width"].stringValue
+                        let joke = IMDB(imageURL: imageURL, width: width)
+                        IMDB.append(IMDB)
+                    }
+                    return
+                }
+            }
+        }
+        showingAlert = true
+    }
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct IMDB: Identifiable {
+        let height = UUID()
+        var imageURL: String
+        var width: String
     }
 }
