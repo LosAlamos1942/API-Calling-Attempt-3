886ce5d2e1213569037dcb28644de063ec8c5ced
Nerto Kageh <kagehnerto@yahoo.com>
2022-02-17 23:35:02 -0600

    Initial Commit

---
+++ API Calling Attempt 3/API_Calling_Attempt_3App.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_Attempt_3App.swift
+//  API Calling Attempt 3
+//
+//  Created by Student on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_Attempt_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
